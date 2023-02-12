e9523b2d33cba2f0226758d91c957c2aac6fa57a
MarcosPacheco19 <65028325+MarcosPacheco19@users.noreply.github.com>
2023-02-12 11:54:31 -0500

    Instalacion de Material y Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GestionComponent } from './pages/clientes/gestion/gestion.component';
+import { GestionClientesComponent } from './pages/clientes/gestion-clientes/gestion-clientes.component';
+import { GestionProductosComponent } from './pages/productos/gestion-productos/gestion-productos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GestionComponent,
+    GestionClientesComponent,
+    GestionProductosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
